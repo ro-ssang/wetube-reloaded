2dcb7e2a82d2feefeb394de1eb26ec10e75103f5
rossa <fashionfish@naver.com>
2021-04-13 15:39:00 +0900

    Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -4,8 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
   next();
 };
 
@@ -13,7 +22,14 @@ const handleHome = (req, res) => {
   return res.send("I love middlewares.");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server Listening on http://localhost:4000`);
