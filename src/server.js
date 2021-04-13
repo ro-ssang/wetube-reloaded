cd46102a6004587159ddc37273e324cd788df210
rossa <fashionfish@naver.com>
2021-04-13 14:52:07 +0900

    GET Requests

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`Server Listening on http://localhost:4000`);
 
