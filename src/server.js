de7df60d83c402dcc8473dc253c0d2c01df199dc
rossa <fashionfish@naver.com>
2021-04-13 15:12:22 +0900

    Requests and Responses Recap

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const PORT = 4000;
 const app = express();
 
 const handleHome = (req, res) => {
-  return res.send("I still love you.");
+  return res.send("<h1>I still love you.</h1>");
 };
 const handleLogin = (req, res) => {
-  return res.send("Login here.");
+  return res.send({ messeage: "Login here." });
 };
 
 app.get("/", handleHome);
