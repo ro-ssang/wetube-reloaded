7376cf68caf77ca1584bdc44d801db842e342d3a
rossa <fashionfish@naver.com>
2021-04-13 16:40:39 +0900

    External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,37 +1,25 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("Hello");
 };
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares.");
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
+const login = (req, res) => {
+  return res.send("login");
 };
 
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
-  console.log(`Server Listening on http://localhost:4000`);
+  console.log(`Server Listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
