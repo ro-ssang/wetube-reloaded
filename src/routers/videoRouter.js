a941883b054e6f5575a99030cfe6585b8fae5e0b
rossa <fashionfish@naver.com>
2021-04-14 16:04:46 +0900

    URL Parameters part Two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,8 +4,8 @@ import { deleteVideo, edit, see, upload } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/delete", deleteVideo);
-videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)/edit", edit);
 
 export default videoRouter;
