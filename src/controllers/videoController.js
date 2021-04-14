0a6f6182d36258b4d2147880c8e6e5890e0a0bb8
rossa <fashionfish@naver.com>
2021-04-14 15:54:13 +0900

    URL Parameters part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,18 @@
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = (req, res) => {
+  console.log(req.params);
+  res.send("Delete Video");
+};
 
-export const edit = (req, res) => res.send("Edit Video");
+export const edit = (req, res) => {
+  console.log(req.params);
+  res.send("Edit Video");
+};
 
 export const search = (req, res) => res.send("Seach Video");
 
-export const see = (req, res) => res.send("See Video");
+export const see = (req, res) => {
+  res.send(`Watch Video #${req.params.id}`);
+};
 
 export const trending = (req, res) => res.send("Home Page Videos");
 
