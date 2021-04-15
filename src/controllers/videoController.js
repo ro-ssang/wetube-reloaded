adba814d63109b27a5805c194d6f7bfeb2ab49b7
rossa <fashionfish@naver.com>
2021-04-15 16:30:28 +0900

    Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,13 +12,28 @@ export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const trending = (req, res) => {
   const videos = [
     {
-      title: "Hello",
+      id: 1,
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
     },
     {
-      title: "Video #2",
+      id: 2,
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
     },
     {
-      title: "Whatsup",
+      id: 3,
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
     },
   ];
   res.render("home", { pageTitle: "Home", videos });
