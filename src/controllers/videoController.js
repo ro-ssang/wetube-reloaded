131c0465a5c5186d6aca3ef9f261bd03fa54cc39
rossa <fashionfish@naver.com>
2021-04-15 14:50:31 +0900

    Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,12 +3,12 @@ export const deleteVideo = (req, res) => {
   res.send("Delete Video");
 };
 
-export const edit = (req, res) => res.render("edit");
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 
 export const search = (req, res) => res.send("Seach Video");
 
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 
 export const upload = (req, res) => res.send("Upload Video");
