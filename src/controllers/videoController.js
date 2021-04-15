631ed4f2a64e645ee6c953a031fb5e11a99aba0c
rossa <fashionfish@naver.com>
2021-04-15 14:26:26 +0900

    Partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,19 +3,11 @@ export const deleteVideo = (req, res) => {
   res.send("Delete Video");
 };
 
-export const edit = (req, res) => {
-  res.send(
-    `<!DOCTYPE html><html><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2021 - All rights reserved</footer></body></html>`
-  );
-};
+export const edit = (req, res) => res.render("edit");
 
 export const search = (req, res) => res.send("Seach Video");
 
-export const see = (req, res) => {
-  res.send(
-    `<!DOCTYPE html><html><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2021 - All rights reserved</footer></body></html>`
-  );
-};
+export const see = (req, res) => res.render("watch");
 
 export const trending = (req, res) => res.render("home");
 
