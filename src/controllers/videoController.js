96022aa797867506776203c903cf908cc5c8068d
rossa <fashionfish@naver.com>
2021-04-16 11:59:53 +0900

    Array Database part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,7 +5,7 @@ let videos = [
     rating: 5,
     comments: 2,
     createdAt: "2 minutes ago",
-    views: 59,
+    views: 1,
   },
   {
     id: 2,
@@ -34,14 +34,14 @@ export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 
 export const search = (req, res) => res.send("Seach Video");
 
-export const see = (req, res) => {
-  const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}` });
-};
-
 export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const upload = (req, res) => res.send("Upload Video");
+
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { deleteVideo, edit, see, upload } from "../controllers/videoController";
+import {
+  deleteVideo,
+  edit,
+  upload,
+  watch,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/:id(\\d+)/edit", edit);
 
