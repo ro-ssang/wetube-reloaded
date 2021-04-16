ceb829f981578464b0453f513a7fed9fad17e2e0
rossa <fashionfish@naver.com>
2021-04-16 13:39:43 +0900

    Edit Video part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,7 +31,12 @@ export const getEdit = (req, res) => {
   return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
 
 export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
@@ -12,6 +12,7 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
