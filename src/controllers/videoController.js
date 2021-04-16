5a8770d0c60e37c9dee4e2fc33cf6627fcd65cbd
rossa <fashionfish@naver.com>
2021-04-16 11:42:32 +0900

    Array Database part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,30 @@
+let videos = [
+  {
+    id: 1,
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+  },
+  {
+    id: 2,
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+  },
+  {
+    id: 3,
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+  },
+];
+
 export const deleteVideo = (req, res) => {
   console.log(req.params);
   res.send("Delete Video");
@@ -7,36 +34,14 @@ export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 
 export const search = (req, res) => res.send("Seach Video");
 
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 
 export const trending = (req, res) => {
-  const videos = [
-    {
-      id: 1,
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-    },
-    {
-      id: 2,
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-    },
-    {
-      id: 3,
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-    },
-  ];
-  res.render("home", { pageTitle: "Home", videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const upload = (req, res) => res.send("Upload Video");
