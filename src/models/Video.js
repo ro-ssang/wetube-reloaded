9db9213cfd860ab454b9ca8684ce58aca9232335
rossa <fashionfish@naver.com>
2021-04-18 01:06:22 +0900

    More Schema

--- src/models/Video.js
+++ src/models/Video.js
@@ -4,10 +4,14 @@ const videoSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
+    trim: true,
+    maxLength: 80,
   },
   description: {
     type: String,
     required: true,
+    trim: true,
+    minLength: 20,
   },
   hashtags: [{ type: String }],
   meta: {
