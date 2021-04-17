44e2436f54f9d2fc37c4e51fcc353eac217e12fd
rossa <fashionfish@naver.com>
2021-04-17 15:31:24 +0900

    Connecting to Mongo

---
+++ src/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleError = (error) => console.log("DB Error", error);
+const handleOpen = () => console.log("Connected to DB");
+
+db.on("error", handleError);
+db.once("open", handleOpen);
