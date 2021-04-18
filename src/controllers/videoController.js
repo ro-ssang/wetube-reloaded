921bf82e03505e8b600f076b2093f623f0144075
rossa <fashionfish@naver.com>
2021-04-18 10:49:08 +0900

    Edit Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,12 @@
 import Video from "../models/Video";
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
 export const getUpload = (req, res) => {
@@ -44,5 +48,8 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
   return res.render("watch", { pageTitle: video.title, video });
 };
