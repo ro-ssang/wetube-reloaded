4418fa7afa8ddc398d7d3e92c70c2b796e5b7f4a
rossa <fashionfish@naver.com>
2021-04-19 15:05:44 +0900

    Creating Account part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,5 @@
+import User from "../models/User";
+
 export const edit = (req, res) => res.send("Edit User");
 
 export const getJoin = (req, res) =>
@@ -7,9 +9,16 @@ export const login = (req, res) => res.send("Login");
 
 export const logout = (req, res) => res.send("Logout");
 
-export const postJoin = (req, res) => {
-  console.log(req.body);
-  return res.end();
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
 };
 
 export const remove = (req, res) => res.send("Remove User");
