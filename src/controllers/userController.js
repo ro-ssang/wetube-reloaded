aa7c052500da743ce158d26f029d66f67a09c45c
rossa <fashionfish@naver.com>
2021-04-20 11:37:54 +0900

    Login part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,8 @@ export const edit = (req, res) => res.send("Edit User");
 export const getJoin = (req, res) =>
   res.render("join", { pageTitle: "Create Account" });
 
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
 
 export const logout = (req, res) => res.send("Logout");
 
@@ -24,14 +25,34 @@ export const postJoin = async (req, res) => {
       errorMessage: "This username/email is already taken.",
     });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Create Account",
+      errorMessage: error._message,
+    });
+  }
+};
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const usernameExists = await User.exists({ username });
+  if (!usernameExists) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  // check if password correct.
+  return res.end();
 };
 
 export const remove = (req, res) => res.send("Remove User");
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { getJoin, login, postJoin } from "../controllers/userController";
+import {
+  getJoin,
+  getLogin,
+  postJoin,
+  postLogin,
+} from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
